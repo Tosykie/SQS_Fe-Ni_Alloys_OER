9a6db81211f9f612731092003e8ef94968078657
Nong Wei <47625283+Tosykie@users.noreply.github.com>
2022-08-03 15:04:31 +0800

    Update subvasp.sh

--- OER/O-covered/subvasp.sh
+++ OER/O-covered/subvasp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/BIGDATA1/app/intelcompiler/14.0.2/composer_xe_2013_sp1.2.144/bin/intel64:/BIGDATA1/app/intelcompiler/14.0.2/composer_xe_2013_sp1.2.144/compiler/lib/intel64:$LD_LIBRARY_PATH
 source /BIGDATA1/app/intelcompiler/14.0.2/composer_xe_2013_sp1.2.144/mkl/bin/mklvars.sh intel64
-VASP_PATH=/BIGDATA2/sysu_liyan_2/softwares/VASP/vasp5.4-v3/vasp.5.4.4/bin
+VASP_PATH=~/softwares/VASP/vasp5.4-v3/vasp.5.4.4/bin
 export PATH=${VASP_PATH}:$PATH
 
 yhrun -N $SLURM_NNODES -n $SLURM_NTASKS vasp_$1
