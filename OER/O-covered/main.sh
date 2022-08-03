79984facbaa2e1b25a02292f4c1076a1d4e66604
Nong Wei <47625283+Tosykie@users.noreply.github.com>
2022-08-03 15:04:07 +0800

    Update main.sh

--- OER/O-covered/main.sh
+++ OER/O-covered/main.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH -J NiFe0.75OER
+#SBATCH -J SQS-FeNi-OER
 #SBATCH -p bigdata
 #SBATCH -N 3
 #SBATCH --ntasks-per-node=24
-potdir=/BIGDATA2/sysu_liyan_2/softwares/VASP/pot/pot_PBE.54
+potdir=~/softwares/VASP/pot/pot_PBE.54
 
 source functions.sh
 #---------------------------- Calculations ----------------------------#
-#xsd2pos.py
+xsd2pos.py
 elements="Fe Ni O H" 
 echo "Start: `date`" >> RunTime.log
 POSCAR_input="POSCAR_*.vasp"
 source opt.sh
 source scf.sh
-#source dos.sh
+source dos.sh
 source bader.sh
 source zpe.sh
-echo "Start: `date`" >> RunTime.log
+echo "End: `date`" >> RunTime.log
